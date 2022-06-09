4fc586c1b0ef36e5309f65d402f23e6cf0f31b87
Yulian <yulianloaiza@gmail.com>
2022-06-09 16:07:55 -0600

    Imagenes con altura maxima en ContentCard

--- src/Component/ContentCard/index.js
+++ src/Component/ContentCard/index.js
@@ -2,9 +2,6 @@ import {FaMapMarkerAlt } from "react-icons/fa";
 //Pendiente hacerlo responsive
 
 //Va a llevar borde? <div className={`border ${theme.productBorder}`}>
-//pendiente hacer el tamaño de los usbtitulos mas pequeño
-//pendiente agregar icono de google maps antes de ubicacion
-//Pendiente ver si se tiene que modificar el tamano de la imagen
 function ContentCard({
   image,
   mainText,
@@ -13,14 +10,14 @@ function ContentCard({
   location,
   onClick,
 }) {
-  //<div className="flex gap-4 content-start w-1/4 bg-idle-grey">
-  //Tal vez forzar un tamaño para el div que guarda la imagen?
+  //Pendiente, que las imagenes se vean a full, o que las imagenes sean mas widescreen y se rellene el
+  //espacio en blanco de alguna manera
   return (
     <>
       <div className="flex gap-4 content-start w-1/4 bg-idle-grey">
         <div>          
           <div>
-            <img src={image} alt={mainText} />
+            <img className="w-full h-60 object-cover" src={image} alt={mainText} />
           </div>
           <div className="p-4 text-left">
             <p className="text-subtitle-grey"> Nombre </p>
@@ -29,8 +26,6 @@ function ContentCard({
             <p className="text-lg pb-2">{secondaryText}</p>
             <p className="text-subtitle-grey"> Ubicación </p>
             <p className="text-lg flex gap-2"><FaMapMarkerAlt color="EA4335" className=" mt-1"/>{location}</p>
-
-            
           </div>
         </div>
       </div>
