1e9b332ad728c8d7d8ab8d625a493396e23c4289
Yulian <yulianloaiza@gmail.com>
2022-06-19 18:25:23 -0600

    intentoComponente con Booleano

--- src/Pages/AnimalInfo/index.js
+++ src/Pages/AnimalInfo/index.js
@@ -16,16 +16,12 @@ function AnimalInfo() {
             En realidad se debe agarrrar del backend*/
   const imgArray = [
     "https://icalmpet.com/wp-content/uploads/iCalm-Pet-Solutions-Dog.jpg",
-    "https://imageserver.petsbest.com/marketing/blog/meeting-new-dogs.jpg"  
+    "https://imageserver.petsbest.com/marketing/blog/meeting-new-dogs.jpg",        
+    "https://petapixel.com/assets/uploads/2022/06/Breathtaking-Photos-of-Airborne-Dogs-Highlighted-by-Colorful-Holi-Paint07-800x800.jpg",
+    "https://gray-kcbd-prod.cdn.arcpublishing.com/resizer/SQif8hGoA6PNTFUsqV5sEpUbUBU=/800x800/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/gray/STCTNNN5SBCQ7L34YV7WD36APA.jpg",
+    "https://d2zp5xs5cp8zlg.cloudfront.net/image-35477-800.jpg",
+    "https://www.dogstrust.org.uk/dogimages/1120385_ginger_20220427084245_ginger-summer-pic_800.jpg",
   ];
-  // const imgArray = [
-  //   "https://icalmpet.com/wp-content/uploads/iCalm-Pet-Solutions-Dog.jpg",
-  //   "https://imageserver.petsbest.com/marketing/blog/meeting-new-dogs.jpg",
-  //   "https://petapixel.com/assets/uploads/2022/06/Breathtaking-Photos-of-Airborne-Dogs-Highlighted-by-Colorful-Holi-Paint07-800x800.jpg",
-  //   "https://gray-kcbd-prod.cdn.arcpublishing.com/resizer/SQif8hGoA6PNTFUsqV5sEpUbUBU=/800x800/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/gray/STCTNNN5SBCQ7L34YV7WD36APA.jpg",
-  //   "https://d2zp5xs5cp8zlg.cloudfront.net/image-35477-800.jpg",
-  //   "https://www.dogstrust.org.uk/dogimages/1120385_ginger_20220427084245_ginger-summer-pic_800.jpg",
-  // ];
   return (
     <>
       <div className="bg-light-gold">
