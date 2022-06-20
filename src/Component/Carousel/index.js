508741f9ae7cdd2d3e75fa58a684ff8f8608c155
Yulian <yulianloaiza@gmail.com>
2022-06-19 18:05:22 -0600

    Intento ejemplo mínimo

--- src/Component/Carousel/index.js
+++ src/Component/Carousel/index.js
@@ -37,92 +37,30 @@ function Carousel({ images }) {
         {/*carousel-inner es todo lo que va a estar afectado por el cambio en flechas 
         Aca estan las slides*/}
         <div class="carousel-inner overflow-hidden grid grid-flow-rows-3 justify-center">
-          {images.map((i, index) => {
-            return (              
-              <>
-                {this.index === 0 ? (
-                  <div class="carousel-item active content-center max-w-max">
-                    <img
-                      src={i}
-                      className="max-h-96 max-w-96 rounded-t"
-                      alt="Imagen principal"
-                    />
-                  </div>
-                ) : (
-                  <div class="carousel-item content-center max-w-max">
-                    <img
-                      src={i}
-                      className="max-h-96 max-w-96 rounded-t"
-                      alt="Imagen principal"
-                    />
-                  </div>
-                )}
-                {/*Thumbnail slider */}
-                <div className="relative w-full bg-gray-400 max-w-fit rounded-b">
-                  <div
-                    ref={sliderRef}
-                    className="w-full flex gap-4 overflow-x-scroll scroll-smooth scrollbar-hide p-2"
-                  >
-                    <img
-                      src={i}
-                      className="max-h-20 rounded"
-                      alt="Visualización previa"
-                      data-bs-target="#imageCarousel"
-                      data-bs-slide-to={index.toString()}
-                    />
-                    {/*Botones prev y next*/}
-                    <button
-                      className="absolute top-8 left-0 text-3xl font-bold carousel-dark bg-component-shadow rounded-full opacity-80"
-                      onClick={() => buttonScroll("back")}
-                    >
-                      <span
-                        class="carousel-control-prev-icon inline-block bg-no-repeat max-h-5"
-                        aria-hidden="true"
-                      ></span>
-                    </button>
-                    <button
-                      className="absolute top-8 right-0 text-3xl carousel-dark float-right bg-component-shadow rounded-full opacity-80"
-                      onClick={() => buttonScroll("forward")}
-                    >
-                      <span
-                        class="carousel-control-next-icon inline-block bg-no-repeat max-h-5"
-                        aria-hidden="true"
-                      ></span>
-                    </button>
-                  </div>
-                </div>
-                {/*Termina Thumbnail slider */}
-              </>
-            );
-          })}        
+
+          {images.loaded
+            ? images.map((i, index) => {
+                return (
+                  <>
+                    {images.loaded ? (
+                      this.index === 0 ? (
+                        <div key={i}> Something went wrong {i} </div>
+                      ) : (
+                        <div key={i}> Everything in the world is fine {i} </div>
+                      )
+                    ) : (
+                      <div>Loading...</div>
+                    )}
+                  </>
+                );
+              })
+            : <div>
+              Cargando...
+            </div>
+             }
         </div>
-        {/*Control atras y siguiente */}
-        <button
-          class="carousel-control-prev carousel-dark absolute top-0 bottom-24 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-          type="button"
-          data-bs-target="#imageCarousel"
-          data-bs-slide="prev"
-        >
-          <span
-            class="carousel-control-prev-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next carousel-dark absolute top-0 bottom-24 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-          type="button"
-          data-bs-target="#imageCarousel"
-          data-bs-slide="next"
-        >
-          <span
-            class="carousel-control-next-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+        <p> Hola jaja</p>
       </div>
-      {/*Termina Carrusel */}
     </>
   );
 }
--- src/Pages/AnimalInfo/index.js
+++ src/Pages/AnimalInfo/index.js
@@ -3,27 +3,29 @@ import Footer from "../../Component/Footer";
 import Carousel from "../../Component/Carousel";
 //Cambiar el nombre cuando se resuelva la duda con el profe del nombre de la ruta.
 
-{
+
   /*
 Aca se usa un arreglo de imagenes solo para fines ilustrativos
 En realidad se debe agarrrar del backend.
 El texto tambien se deberia jalar del backend
           */
-}
+
 
 function AnimalInfo() {
-  {
-    /*arreglo de imagenes solo para fines ilustrativos
+  /*arreglo de imagenes solo para fines ilustrativos
             En realidad se debe agarrrar del backend*/
-  }
   const imgArray = [
     "https://icalmpet.com/wp-content/uploads/iCalm-Pet-Solutions-Dog.jpg",
-    "https://imageserver.petsbest.com/marketing/blog/meeting-new-dogs.jpg",
-    "https://petapixel.com/assets/uploads/2022/06/Breathtaking-Photos-of-Airborne-Dogs-Highlighted-by-Colorful-Holi-Paint07-800x800.jpg",
-    "https://gray-kcbd-prod.cdn.arcpublishing.com/resizer/SQif8hGoA6PNTFUsqV5sEpUbUBU=/800x800/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/gray/STCTNNN5SBCQ7L34YV7WD36APA.jpg",
-    "https://d2zp5xs5cp8zlg.cloudfront.net/image-35477-800.jpg",
-    "https://www.dogstrust.org.uk/dogimages/1120385_ginger_20220427084245_ginger-summer-pic_800.jpg",
+    "https://imageserver.petsbest.com/marketing/blog/meeting-new-dogs.jpg"  
   ];
+  // const imgArray = [
+  //   "https://icalmpet.com/wp-content/uploads/iCalm-Pet-Solutions-Dog.jpg",
+  //   "https://imageserver.petsbest.com/marketing/blog/meeting-new-dogs.jpg",
+  //   "https://petapixel.com/assets/uploads/2022/06/Breathtaking-Photos-of-Airborne-Dogs-Highlighted-by-Colorful-Holi-Paint07-800x800.jpg",
+  //   "https://gray-kcbd-prod.cdn.arcpublishing.com/resizer/SQif8hGoA6PNTFUsqV5sEpUbUBU=/800x800/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/gray/STCTNNN5SBCQ7L34YV7WD36APA.jpg",
+  //   "https://d2zp5xs5cp8zlg.cloudfront.net/image-35477-800.jpg",
+  //   "https://www.dogstrust.org.uk/dogimages/1120385_ginger_20220427084245_ginger-summer-pic_800.jpg",
+  // ];
   return (
     <>
       <div className="bg-light-gold">
