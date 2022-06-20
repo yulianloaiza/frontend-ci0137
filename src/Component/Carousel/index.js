1f9e7af00b0356b4acb70a2dd969ad6616f1b1f7
Yulian <yulianloaiza@gmail.com>
2022-06-19 18:39:44 -0600

    Componente de carrusel.

--- src/Component/Carousel/index.js
+++ src/Component/Carousel/index.js
@@ -3,12 +3,10 @@ import "tw-elements";
 
 /*
 Al igual que en ContentCard, aca se suben fotos de 800x800 pixeles
-Se puede agregar bool para indicadores o no. Pendiente ver si se pasa como parametro opcional
-las imagenes con thumbnails para previews
-Pendiente acomodar botones e indicador en terminos de posicion*/
+*/
 function Carousel({ images }) {
   const sliderRef = useRef();
-  let firstItem=true;
+  let firstItem = true;
 
   //Botones del slider para moverse a la izquierda o derecha.
   function buttonScroll(direction) {
@@ -30,18 +28,16 @@ function Carousel({ images }) {
       {/*Inicia Carrusel */}
       <div
         id="imageCarousel"
-        /*class="carousel slide relative"
-        Pendiente ver como poder arreglar esa animacion entre slides. Antes se mostraba mal*/
         class="carousel relative "
         data-bs-interval="false"
       >
-        {/*carousel-inner es todo lo que va a estar afectado por el cambio en flechas 
-        Aca estan las slides*/}
+        {/*carousel-inner es todo lo que va a estar afectado por el cambio en flechas. Aca estan las slides.
+        Se hace map en los elementos internos de las divisiones*/}
         <div class="carousel-inner overflow-hidden grid grid-flow-rows-3 justify-center">
-          {images.map((i, index) => {
-            return (              
+          {images.map((i) => {
+            return (
               <>
-                {firstItem === true ? (                                    
+                {firstItem === true ? (
                   <div class="carousel-item active content-center max-w-max">
                     <img
                       src={i}
@@ -58,47 +54,48 @@ function Carousel({ images }) {
                     />
                   </div>
                 )}
-                {console.log(firstItem)}
-                {firstItem=false}
-                
-                {/*Thumbnail slider */}
-                <div className="relative w-full bg-gray-400 max-w-fit rounded-b">
-                  <div
-                    ref={sliderRef}
-                    className="w-full flex gap-4 overflow-x-scroll scroll-smooth scrollbar-hide p-2"
-                  >
-                    <img
-                      src={i}
-                      className="max-h-20 rounded"
-                      alt="Visualización previa"
-                      data-bs-target="#imageCarousel"
-                      data-bs-slide-to={index.toString()}
-                    />
-                    {/*Botones prev y next*/}
-                    <button
-                      className="absolute top-8 left-0 text-3xl font-bold carousel-dark bg-component-shadow rounded-full opacity-80"
-                      onClick={() => buttonScroll("back")}
-                    >
-                      <span
-                        class="carousel-control-prev-icon inline-block bg-no-repeat max-h-5"
-                        aria-hidden="true"
-                      ></span>
-                    </button>
-                    <button
-                      className="absolute top-8 right-0 text-3xl carousel-dark float-right bg-component-shadow rounded-full opacity-80"
-                      onClick={() => buttonScroll("forward")}
-                    >
-                      <span
-                        class="carousel-control-next-icon inline-block bg-no-repeat max-h-5"
-                        aria-hidden="true"
-                      ></span>
-                    </button>
-                  </div>
-                </div>
-                {/*Termina Thumbnail slider */}
+                {(firstItem = false)}
               </>
             );
-          })}        
+          })}
+          {/*Thumbnail slider */}
+          <div className="relative w-full bg-gray-400 max-w-fit rounded-b">
+            <div
+              ref={sliderRef}
+              className="w-full flex gap-4 overflow-x-scroll scroll-smooth scrollbar-hide p-2"
+            >
+              {images.map((i, index) => {
+                return (
+                  <img
+                    src={i}
+                    className="max-h-20 rounded"
+                    alt="Visualización previa"
+                    data-bs-target="#imageCarousel"
+                    data-bs-slide-to={index.toString()}
+                  />
+                );
+              })}
+              {/*Botones prev y next*/}
+              <button
+                className="absolute top-8 left-0 text-3xl font-bold carousel-dark bg-component-shadow rounded-full opacity-80"
+                onClick={() => buttonScroll("back")}
+              >
+                <span
+                  class="carousel-control-prev-icon inline-block bg-no-repeat max-h-5"
+                  aria-hidden="true"
+                ></span>
+              </button>
+              <button
+                className="absolute top-8 right-0 text-3xl carousel-dark float-right bg-component-shadow rounded-full opacity-80"
+                onClick={() => buttonScroll("forward")}
+              >
+                <span
+                  class="carousel-control-next-icon inline-block bg-no-repeat max-h-5"
+                  aria-hidden="true"
+                ></span>
+              </button>
+            </div>
+          </div>
         </div>
         {/*Control atras y siguiente */}
         <button
@@ -111,7 +108,6 @@ function Carousel({ images }) {
             class="carousel-control-prev-icon inline-block bg-no-repeat"
             aria-hidden="true"
           ></span>
-          <span class="visually-hidden">Previous</span>
         </button>
         <button
           class="carousel-control-next carousel-dark absolute top-0 bottom-24 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
@@ -123,10 +119,8 @@ function Carousel({ images }) {
             class="carousel-control-next-icon inline-block bg-no-repeat"
             aria-hidden="true"
           ></span>
-          <span class="visually-hidden">Next</span>
         </button>
       </div>
-      {/*Termina Carrusel */}
     </>
   );
 }
