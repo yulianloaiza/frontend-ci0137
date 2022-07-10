feab123b7e7b016061c346f5b6eb7925f2bdf7b7
Yulian <yulianloaiza@gmail.com>
2022-07-10 08:59:27 -0600

    OT: Boton con volver hacia atras o ruta. Titulos. Mensaje de exito en formulario. Mejoras en title

--- src/Component/Title/index.js
+++ src/Component/Title/index.js
@@ -1,11 +1,12 @@
-function Title({titleText}) {
+function Title({ titleText }) {
   return (
     <div
-      className="titles gap-8 justify-items-left py-4 mb-8 
-        grid grid-cols-1 sm:grid-cols-2  md:grid-cols-4
+      className="titles text-left py-4 mb-8         
         px-4 md:px-8 lg:px-20 text-navy-blue"
     >
-      <p><strong>{titleText}</strong></p>
+      <p>
+        <strong>{titleText}</strong>
+      </p>
     </div>
   );
 }
--- src/Pages/AnimalInfo/index.js
+++ src/Pages/AnimalInfo/index.js
@@ -1,8 +1,9 @@
+import Mixpanel from "../../services/mixpanel";
 import Header from "../../Component/Header";
 import Footer from "../../Component/Footer";
 import Carousel from "../../Component/Carousel";
 import Button from "../../Component/Button";
-import Mixpanel from "../../services/mixpanel";
+import Title from "../../Component/Title";
 //Cambiar el nombre cuando se resuelva la duda con el profe del nombre de la ruta.
 
 /*
@@ -26,6 +27,21 @@ function AnimalInfo() {
     <>
       <div className="bg-light-gold">
         <Header />
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="md:text-right ">
+            <Title titleText={"NOMBREPERRO"} />
+          </div>
+
+          <div className="px-4 md:pr-8 lg:pr-20 pb-4 mb-3 self-center md:text-right">
+            <Button
+              text="Regresar"
+              width={"1/2 md:w-3/4 xl:w-1/4 "}
+              className="px-4 md:px-8 lg:px-20"
+              goBack
+            />
+          </div>
+        </div>
+
         <div
           className="px-4 md:px-8 lg:px-20 pb-4 mb-8
           grid grid-cols-1  
@@ -78,7 +94,13 @@ function AnimalInfo() {
               </p>
             </div>
             <div className="text-center mb-6 ">
-              <Button onClick={() => {Mixpanel.track(Mixpanel.TYPES.GO_TO_ADOPTION_FORM); }} text="Adoptar" width={"full"} />
+              <Button
+                onClick={() => {
+                  Mixpanel.track(Mixpanel.TYPES.GO_TO_ADOPTION_FORM);
+                }}
+                text="Adoptar"
+                width={"full"}
+              />
             </div>
           </div>
         </div>
