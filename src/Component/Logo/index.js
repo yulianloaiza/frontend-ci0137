9b2341a02c6c9fc8ce03c844eba7aa9862026922
Yulian <yulianloaiza@gmail.com>
2022-07-10 10:13:09 -0600

    OT: Favicon y logo en header

--- src/Component/Logo/index.js
+++ src/Component/Logo/index.js
@@ -1,14 +1,13 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-//TODO arreglar a que este logo sea el de la app, tal ves modificar tamaño default y que el src este en folder public
-function Logo({ width, height, link='/home' }) {
-  let navigate = useNavigate(); 
+function Logo({ width, height, link = "/home" }) {
+  let navigate = useNavigate();
   return (
     <img
       className={`${width || "w-20"} ${height || "h-20"} cursor-pointer`}
-      onClick={()=> navigate(link)}
+      onClick={() => navigate(link)}
       alt="Logo"
-      src="https://bit.ly/logo-ci0137"
+      src="https://drive.google.com/uc?id=1G57Uh9PU8sWEK9BI_VP1clR9cEvanE5X"
     />
   );
 }
